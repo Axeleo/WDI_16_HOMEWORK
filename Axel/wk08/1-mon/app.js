df223adbe842ae1c95f6cfb7d2f08d63c4031fa5
Axel <dlaxel@hotmail.com>
2018-08-06 17:11:07 +1000

    added commit 7

--- Axel/wk08/1-mon/app.js
+++ Axel/wk08/1-mon/app.js
@@ -1,7 +1,7 @@
 
 var brushBox = $(".brush-box")
 
-$("form").submit(function () {
+$(".color-form").submit(function () {
   var setColor = $(".color-input")[0].value
   event.preventDefault()
   brushBox[0].style.backgroundColor = setColor
@@ -11,3 +11,25 @@ $("form").submit(function () {
 $(".wrapper").mouseover(ev => {
   event.target.style.backgroundColor = brushBox[0].style.backgroundColor
 })
+
+$(".OMDB-background-form").submit(function () {
+  event.preventDefault()
+  OMDBRetrive()
+})
+
+function OMDBRetrive() {
+  let options = {
+    method:'GET',
+    url:`http://omdbapi.com/`,
+    dataType: 'json',
+    data: {
+      s: $(".OMDB-search-input")[0].value,
+      apikey: '2f6435d9'
+    }
+  }
+  $.ajax(options).done(backgroundSet)
+}
+
+const backgroundSet = response => {
+  $('body').css('background-image', `url(${response.Search[0].Poster})`)
+}
