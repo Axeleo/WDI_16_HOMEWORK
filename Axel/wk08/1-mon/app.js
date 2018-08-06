e8764b16adf82b8fd32344d278da5299ce65b101
Axel <dlaxel@hotmail.com>
2018-08-06 16:27:19 +1000

    added commit 1 of task

---
+++ Axel/wk08/1-mon/app.js
@@ -0,0 +1,8 @@
+
+var brushBox = $(".brush-box")
+
+$("form").submit(function () {
+  var setColor = $(".color-input")[0].value
+  event.preventDefault()
+  brushBox[0].style.backgroundColor = setColor
+})
